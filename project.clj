7d5416aecd21233666cd4d7f2548f2d690de8b16
Andrios <andriosr@gmail.com>
2021-12-14 19:45:59 -0300

    bump log4j deps to fix 0-day issue

--- project.clj
+++ project.clj
@@ -16,8 +16,8 @@
                                       [ch.qos.logback/logback-core    "1.2.3"]]}
              :log4j12 {:dependencies [[org.slf4j/slf4j-log4j12 "1.7.30"]
                                       [log4j/log4j "1.2.17"]]}
-             :log4j2  {:dependencies [[org.apache.logging.log4j/log4j-api  "2.13.3" :exclusions [org.slf4j/slf4j-api]]
-                                      [org.apache.logging.log4j/log4j-core "2.13.3" :exclusions [org.slf4j/slf4j-api]]
+             :log4j2  {:dependencies [[org.apache.logging.log4j/log4j-api  "2.16.0" :exclusions [org.slf4j/slf4j-api]]
+                                      [org.apache.logging.log4j/log4j-core "2.16.0" :exclusions [org.slf4j/slf4j-api]]
                                       [org.apache.logging.log4j/log4j-slf4j-impl "2.13.3" :exclusions [org.slf4j/slf4j-api]]]}
              :c06 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :c07 {:dependencies [[org.clojure/clojure "1.7.0"]]}
