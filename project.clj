684467850f885fb736083b8175dd5eb2497e3f93
Shantanu Kumar <kumar.shantanu@gmail.com>
2021-12-16 13:57:17 +0530

    fix #7 - bump dependency versions

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
-(defproject cambium/cambium.core "1.1.0"
+(defproject cambium/cambium.core "1.1.1-SNAPSHOT"
   :description "Core module for the Cambium logging API"
   :url "https://github.com/cambium-clojure/cambium.core"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.2.0"
   :pedantic? :warn
-  :dependencies [[org.slf4j/slf4j-api       "1.7.30"]
-                 [org.clojure/tools.logging "1.1.0" :exclusions [org.clojure/clojure]]]
+  :dependencies [[org.slf4j/slf4j-api       "1.7.32"]
+                 [org.clojure/tools.logging "1.2.2" :exclusions [org.clojure/clojure]]]
   :global-vars {*assert* true
                 *warn-on-reflection* true
                 *unchecked-math* :warn-on-boxed}
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :codec-simple {:dependencies [[cambium/cambium.codec-simple "1.0.0"]]}
-             :logback {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]
-                                      [ch.qos.logback/logback-core    "1.2.3"]]}
+             :logback {:dependencies [[ch.qos.logback/logback-classic "1.2.8"]
+                                      [ch.qos.logback/logback-core    "1.2.8"]]}
              :log4j12 {:dependencies [[org.slf4j/slf4j-log4j12 "1.7.30"]
                                       [log4j/log4j "1.2.17"]]}
              :log4j2  {:dependencies [[org.apache.logging.log4j/log4j-api  "2.16.0" :exclusions [org.slf4j/slf4j-api]]
